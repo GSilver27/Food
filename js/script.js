18e8cb2b68d53c8a9eecea54f5d36f6dbb68fb71
Silver <gramakov27@gmail.com>
2023-02-06 19:30:07 +0300

    timer script for promotion section, min css

--- js/script.js
+++ js/script.js
@@ -4,17 +4,17 @@ window.addEventListener('DOMContentLoaded', () => {
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
 
+
+    // СТИЛЬ ПИТАНИЯ. СМЕНА КОНТЕНТА
     function hideTabContent() {
         tabsContent.forEach(tab => {
             tab.classList.add('hide');
             tab.classList.remove('show', 'fade');
         });
-    
 
         tabs.forEach(tab => {
             tab.classList.remove('tabheader__item_active');
         }); 
-
     }
 
     function showTabsContent(i = 0) {
@@ -39,4 +39,59 @@ window.addEventListener('DOMContentLoaded', () => {
         }
    });
 
+   // АКЦИЯ. ТАЙМЕР
+    const deadline = '2023-03-01';
+
+    function getTimeRemaining(endTime) {
+        const t = Date.parse(endTime) - Date.parse(new Date()),
+              days = Math.floor(t / (1000 * 60 * 60 * 24)),
+              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+              minutes = Math.floor((t / 1000 / 60) % 60),
+              seconds = Math.floor((t / 1000) % 60);
+
+        return {
+            'total': t,
+            days,
+            hours,
+            minutes,
+            seconds,
+        };
+    }
+
+    // нули перед числами
+    function getZero(num) {
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
+        } else {
+            return num;
+        }
+    }
+
+    function setClock(selector, endTime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInteraval = setInterval(updateClock, 1000);
+
+        
+
+        function updateClock() {
+            const t = getTimeRemaining(endTime);
+
+            days.innerHTML = getZero(t.days);
+            hours.innerHTML = getZero(t.hours);
+            minutes.innerHTML = getZero(t.minutes);
+            seconds.innerHTML = getZero(t.seconds);
+
+            if (t.total <= 0) {
+                clearInterval(timeInteraval);
+            }
+        }
+
+        updateClock();
+    }
+
+    setClock('.timer', deadline);
 });
