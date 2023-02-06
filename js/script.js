a44a0f1c6cf1e04597151894ce8f000fa80cca62
Silver <gramakov27@gmail.com>
2023-02-06 19:41:18 +0300

    checking current date

--- js/script.js
+++ js/script.js
@@ -43,11 +43,21 @@ window.addEventListener('DOMContentLoaded', () => {
     const deadline = '2023-03-01';
 
     function getTimeRemaining(endTime) {
-        const t = Date.parse(endTime) - Date.parse(new Date()),
-              days = Math.floor(t / (1000 * 60 * 60 * 24)),
-              hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-              minutes = Math.floor((t / 1000 / 60) % 60),
-              seconds = Math.floor((t / 1000) % 60);
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endTime) - Date.parse(new Date());
+
+        // инициализация даты, в зависимости от текущей
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
+            days = Math.floor(t / (1000 * 60 * 60 * 24)),
+            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+            minutes = Math.floor((t / 1000 / 60) % 60),
+            seconds = Math.floor((t / 1000) % 60);
+        }           
 
         return {
             'total': t,
